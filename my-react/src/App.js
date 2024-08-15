9f11c055cd3d147f3536087583ab33f7ef6e942e
kentangkiting <agustinus_bintang@yahoo.com>
2024-08-15 16:58:53 +0700

    initial commit

---
+++ my-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h4>Ini My React</h4>
+    </div>
+  );
+}
+
+export default App;
