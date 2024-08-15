cb6b4d704f9976c9dcad90dc433437398733cc53
kentangkiting <agustinus_bintang@yahoo.com>
2024-08-15 17:07:10 +0700

    initial commit

---
+++ my-vite/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h4>Ini My Vite</h4>
+    </div>
+  );
+}
+export default App;
